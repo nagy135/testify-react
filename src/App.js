c5d17342f66080d578408040094af213fd7a9f76
Viktor Nagy <viktor.nagy1995@gmail.com>
2021-01-27 18:28:01 +0100

    feat: router + components

--- src/App.js
+++ src/App.js
@@ -1,17 +1,38 @@
 import './App.css';
-import Footer from './Footer';
+import Footer from './components/Footer';
+import Navigation from './components/Navigation';
 import Button from '@material-ui/core/Button';
 import Container from '@material-ui/core/Container';
+
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+} from "react-router-dom";
 
 function App() {
     return (
         <Container
             className="App"
             maxWidth="sm">
-            <Button variant="contained" color="primary">
-                Hello World
-            </Button>
-            <Footer/>
+            <Navigation />
+            <Router>
+                <Switch>
+                    <Route
+                        path="/login"
+                        render={() => (
+                            <Button variant="contained" color="primary">
+                                LOGIN
+                            </Button>
+                        )} />
+                    <Route path="/">
+                        <Button variant="contained" color="primary">
+                            NORMAL
+                        </Button>
+                    </Route>
+                </Switch>
+            </Router>
+            <Footer />
         </Container>
     );
 }
---
+++ src/components/Navigation.js
@@ -0,0 +1,12 @@
+import Container from '@material-ui/core/Container';
+
+const Navigation = () => {
+   return (
+        <Container
+            className="App"
+            maxWidth="sm">
+            Navigation content
+        </Container>
+   );
+}
+export default Navigation;
